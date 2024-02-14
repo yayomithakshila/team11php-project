418bca6380be8899e2aeba15207283d55e6ac860
Yayomi Thakshila Premathilaka <yayomifin@outlook.com>
2024-02-14 15:34:14 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+ $title = "Home page";
+ include 'layout-folder/header.php'; ?> 
+
+<div class="section text-left">
+    <div class="row">
+        <div class="textOnVideo col">
+            <h1>Villa King Coco Hikkaduwa</h1>
+            <h3>Discover The Perfect Balance Of Hospitality, And Comfort.</h3>
+            <a href="login.php" class="btn btn-primary btn-header-book mx-auto">Book Now</a>
+        </div>
+    </div>
+</div>
+      
+       
+<?php include 'layout-folder/footer.php'; ?>
+        
